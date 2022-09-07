0157ff8e73e8fbf31359f86861aab146421da6bc
Rahul Patel <rahulhpatel1998@gmail.com>
2022-09-07 13:02:58 -0400

    init

---
+++ src/main/java/edu/ufl/cise/plpfa22/Lexer.java
@@ -0,0 +1,14 @@
+package edu.ufl.cise.plpfa22;
+
+public class Lexer implements ILexer {
+    public Lexer(final String input) {}
+    @Override
+    public IToken next() throws LexicalException {
+        return null;
+    }
+
+    @Override
+    public IToken peek() throws LexicalException {
+        return null;
+    }
+}
