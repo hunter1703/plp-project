153ede686a4bcc7edd437787354291e2a50c276d
Rahul Patel <rahulhpatel1998@gmail.com>
2022-09-27 19:42:01 -0400

    fixed minor bugs

--- src/main/java/edu/ufl/cise/plpfa22/Parser.java
+++ src/main/java/edu/ufl/cise/plpfa22/Parser.java
@@ -1,12 +1,16 @@
 package edu.ufl.cise.plpfa22;
 
 import edu.ufl.cise.plpfa22.IToken.Kind;
+
 import static edu.ufl.cise.plpfa22.IToken.Kind.*;
+
 import edu.ufl.cise.plpfa22.ast.*;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static edu.ufl.cise.plpfa22.IToken.Kind.NUM_LIT;
+import static java.util.Collections.singleton;
 
 public class Parser implements IParser {
     private final ILexer lexer;
@@ -28,7 +32,7 @@ public class Parser implements IParser {
 
     public Parser(ILexer lexer) {
         this.lexer = lexer;
-        for (NON_TERMINALS T: NON_TERMINALS.values()) {
+        for (NON_TERMINALS T : NON_TERMINALS.values()) {
             FIRST_SETS.put(T, getFirstSet(T));
             FOLLOW_SETS.put(T, getFollowSet(T));
         }
@@ -131,62 +135,61 @@ public class Parser implements IParser {
 
     public Statement getStatementAST() throws PLPException {
         IToken nextToken = lexer.peek();
-        IToken firstToken = nextToken;
         Kind kind = nextToken.getKind();
-        Statement statement = null;
+        Statement statement;
         switch (kind) {
             case IDENT -> {
                 t = lexer.next();
                 t = lexer.next();
                 isKind(t, ASSIGN);
-                statement = new StatementAssign(firstToken, new Ident(firstToken), getExpressionAST());
+                statement = new StatementAssign(nextToken, new Ident(nextToken), getExpressionAST());
             }
             case KW_CALL -> {
                 t = lexer.next();
                 t = lexer.next();
                 isKind(t, IDENT);
-                statement = new StatementCall(firstToken, new Ident(t));
+                statement = new StatementCall(nextToken, new Ident(t));
             }
             case QUESTION -> {
                 t = lexer.next();
                 t = lexer.next();
                 isKind(t, IDENT);
-                statement = new StatementInput(firstToken, new Ident(t));
+                statement = new StatementInput(nextToken, new Ident(t));
             }
             case BANG -> {
                 t = lexer.next();
                 Expression expression = getExpressionAST();
-                statement = new StatementOutput(firstToken, expression);
+                statement = new StatementOutput(nextToken, expression);
             }
             case KW_BEGIN -> {
                 t = lexer.next();
                 List<Statement> statements = new ArrayList<Statement>();
                 statement = getStatementAST();
                 statements.add(statement);
-                while(lexer.peek().getKind() == SEMI) {
+                while (lexer.peek().getKind() == SEMI) {
                     t = lexer.next();
                     statements.add(getStatementAST());
                 }
                 t = lexer.next();
                 isKind(t, KW_END);
-                statement = new StatementBlock(firstToken, statements);
+                statement = new StatementBlock(nextToken, statements);
             }
             case KW_IF -> {
                 t = lexer.next();
                 Expression expression = getExpressionAST();
                 t = lexer.next();
                 isKind(t, KW_THEN);
-                statement = new StatementIf(firstToken, expression, getStatementAST());
+                statement = new StatementIf(nextToken, expression, getStatementAST());
             }
             case KW_WHILE -> {
                 t = lexer.next();
                 Expression expression = getExpressionAST();
                 t = lexer.next();
                 isKind(t, KW_DO);
-                statement = new StatementWhile(firstToken, expression, getStatementAST());
+                statement = new StatementWhile(nextToken, expression, getStatementAST());
             }
             default -> {
-                if (FOLLOW_SETS.get(NON_TERMINALS.STATEMENT).contains(kind)) statement = new StatementEmpty(firstToken);
+                if (FOLLOW_SETS.get(NON_TERMINALS.STATEMENT).contains(kind)) statement = new StatementEmpty(nextToken);
                 else throw new SyntaxException();
             }
         }
@@ -196,8 +199,8 @@ public class Parser implements IParser {
     public Expression getExpressionAST() throws PLPException {
         IToken firstToken = lexer.peek();
         Kind kind = firstToken.getKind();
-        Expression expression = null;
-        if (FIRST_SETS.get(NON_TERMINALS.EXPRESSION).contains(kind)) {
+        Expression expression;
+        if (FIRST_SETS.get(NON_TERMINALS.ADDITIVE_EXPRESSION).contains(kind)) {
             expression = getAdditiveExpressionAST();
             Kind nextTokenKind = lexer.peek().getKind();
             while (nextTokenKind == LT || nextTokenKind == GT || nextTokenKind == EQ || nextTokenKind == NEQ || nextTokenKind == LE || nextTokenKind == GE) {
@@ -208,10 +211,11 @@ public class Parser implements IParser {
         } else throw new SyntaxException();
         return expression;
     }
+
     public Expression getAdditiveExpressionAST() throws PLPException {
         IToken firstToken = lexer.peek();
         Kind kind = firstToken.getKind();
-        Expression expression = null;
+        Expression expression;
         if (FIRST_SETS.get(NON_TERMINALS.MULTIPLICATIVE_EXPRESSION).contains(kind)) {
             expression = getMultiplicativeExpressionAST();
             Kind nextTokenKind = lexer.peek().getKind();
@@ -243,7 +247,7 @@ public class Parser implements IParser {
     public Expression getPrimaryExpressionAST() throws PLPException {
         IToken firstToken = lexer.peek();
         Kind kind = firstToken.getKind();
-        Expression expression = null;
+        Expression expression;
         if (kind == IDENT) {
             t = lexer.next();
             expression = new ExpressionIdent(t);
@@ -261,7 +265,7 @@ public class Parser implements IParser {
     public Expression getConstValAST() throws PLPException {
         t = lexer.next();
         Kind kind = t.getKind();
-        Expression constVal = null;
+        Expression constVal;
         switch (kind) {
             case NUM_LIT -> constVal = new ExpressionNumLit(t);
             case STRING_LIT -> constVal = new ExpressionStringLit(t);
@@ -271,56 +275,43 @@ public class Parser implements IParser {
         return constVal;
     }
 
-    private void isKind(IToken t, Kind k) throws  PLPException {
+    private void isKind(IToken t, Kind k) throws PLPException {
         if (t.getKind() != k) throw new SyntaxException();
     }
 
     private Set<Kind> getFollowSet(NON_TERMINALS type) {
         Set<Kind> s = null;
         switch (type) {
-            case PROGRAM -> s = new HashSet<Kind>();
-            case BLOCK -> s = new HashSet<Kind>(List.of(DOT));
-            case STATEMENT -> s = new HashSet<Kind>(List.of(DOT, SEMI, KW_END));
-            case EXPRESSION -> {
-                s = getFollowSet(NON_TERMINALS.STATEMENT);
-                s.addAll(new HashSet<Kind>(List.of(KW_THEN, KW_DO, RPAREN)));
-            }
-            case ADDITIVE_EXPRESSION -> {
-                s = getFollowSet(NON_TERMINALS.EXPRESSION);
-                s.addAll(new HashSet<Kind>(List.of(LT, GT, EQ, NEQ, LE, GE)));
-            }
-            case MULTIPLICATIVE_EXPRESSION -> {
-                s = getFollowSet(NON_TERMINALS.ADDITIVE_EXPRESSION);
-                s.addAll(new HashSet<Kind>(List.of(PLUS, MINUS)));
-            }
-            case PRIMARY_EXPRESSION -> {
-                s = getFollowSet(NON_TERMINALS.MULTIPLICATIVE_EXPRESSION);
-                s.addAll(new HashSet<Kind>(List.of(TIMES, DIV, MOD)));
-            }
-            case CONST_VAL -> {
-                s = getFollowSet(NON_TERMINALS.PRIMARY_EXPRESSION);
-                s.addAll(new HashSet<Kind>(List.of(COMMA, SEMI)));
-            }
-        };
+            case PROGRAM -> s = Collections.emptySet();
+            case BLOCK -> s = Collections.singleton(DOT);
+            case STATEMENT -> s = union(getFollowSet(NON_TERMINALS.BLOCK), List.of(SEMI, KW_END));
+            case EXPRESSION -> s = union(getFollowSet(NON_TERMINALS.STATEMENT), List.of(KW_THEN, KW_DO, RPAREN));
+            case ADDITIVE_EXPRESSION -> s = union(getFollowSet(NON_TERMINALS.EXPRESSION), List.of(LT, GT, EQ, NEQ, LE, GE));
+            case MULTIPLICATIVE_EXPRESSION -> s = union(getFollowSet(NON_TERMINALS.ADDITIVE_EXPRESSION), List.of(PLUS, MINUS));
+            case PRIMARY_EXPRESSION -> s = union(getFollowSet(NON_TERMINALS.MULTIPLICATIVE_EXPRESSION), List.of(TIMES, DIV, MOD));
+            case CONST_VAL -> s = union(getFollowSet(NON_TERMINALS.PRIMARY_EXPRESSION), List.of(COMMA, SEMI));
+        }
         return s;
     }
 
     private Set<Kind> getFirstSet(NON_TERMINALS type) {
         Set<Kind> s = null;
         switch (type) {
-            case CONST_VAL -> s = new HashSet<Kind>(List.of(NUM_LIT, STRING_LIT, BOOLEAN_LIT));
-            case PRIMARY_EXPRESSION -> s = new HashSet<Kind>(List.of(IDENT, NUM_LIT, STRING_LIT, BOOLEAN_LIT, LPAREN));
+            case CONST_VAL -> s = new HashSet<>(List.of(NUM_LIT, STRING_LIT, BOOLEAN_LIT));
+            case PRIMARY_EXPRESSION -> s = union(singleton(IDENT), getFirstSet(NON_TERMINALS.CONST_VAL), singleton(LPAREN));
             case MULTIPLICATIVE_EXPRESSION -> s = getFirstSet(NON_TERMINALS.PRIMARY_EXPRESSION);
             case ADDITIVE_EXPRESSION -> s = getFirstSet(NON_TERMINALS.MULTIPLICATIVE_EXPRESSION);
             case EXPRESSION -> s = getFirstSet(NON_TERMINALS.ADDITIVE_EXPRESSION);
-            case STATEMENT -> s = new HashSet<Kind>(List.of(IDENT, KW_CALL, QUESTION, BANG, KW_BEGIN, KW_IF, KW_WHILE));
-            case BLOCK -> {
-                s = getFirstSet(NON_TERMINALS.STATEMENT);
-                s.addAll(new HashSet<Kind>(List.of(KW_CONST, KW_VAR, KW_PROCEDURE)));
-            }
-            case PROGRAM -> getFirstSet(NON_TERMINALS.BLOCK);
+            case STATEMENT -> s = new HashSet<>(List.of(IDENT, KW_CALL, QUESTION, BANG, KW_BEGIN, KW_IF, KW_WHILE));
+            case BLOCK -> s = union(getFirstSet(NON_TERMINALS.STATEMENT), List.of(KW_CONST, KW_VAR, KW_PROCEDURE));
+            case PROGRAM -> s = getFirstSet(NON_TERMINALS.BLOCK);
         }
         return s;
+    }
+
+    @SafeVarargs
+    private static <T> Set<T> union(Collection<T>... collections) {
+        return Arrays.stream(collections).flatMap(Collection::stream).collect(Collectors.toSet());
     }
 
 }
